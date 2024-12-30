4044f3e07081e64b007a09fc0cbe44f292d070db
artem <artem.dos.25@gmail.com>
2024-12-30 21:35:54 +0200

    added body to class ListUsageExample and main method

--- src/ListsUsageExample.java
+++ src/ListsUsageExample.java
@@ -1,2 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class ListsUsageExample {
+
+    public static void countOccurance(List<String> words, String target){
+        System.out.println(words);
+        int counter = 0;
+        for(int i = 0; i < words.toArray().length; i++){
+            if(target.equals(words.get(i))){
+                counter++;
+            }
+        }
+        System.out.println("words " + target + " were counted " + counter + " times");
+    }
+
+    public static void toList(int[] array){
+        List <Integer> numbers = new ArrayList<>();
+        for(int i = 0; i < array.length; i++){
+            numbers.add(array[i]);
+        }
+
+        System.out.print("Array: ");
+        for(Object i : array){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("List: " + numbers);
+    }
+
+    public static List<Integer> findUnique(List<Integer> numbers){
+        List<Integer> uniqueNumbers = new ArrayList<>();
+        for (int i = 0; i < numbers.toArray().length; i++) {
+            if(!uniqueNumbers.contains(numbers.get(i))){
+                uniqueNumbers.add(numbers.get(i));
+            }
+        }
+        return uniqueNumbers;
+    }
+
+    public static void calcOccurance(List<String> words){
+        List<String> uniqueNumbers = new ArrayList<>();
+        for (int i = 0; i < words.toArray().length; i++) {
+            if(!uniqueNumbers.contains(words.get(i))){
+                uniqueNumbers.add(words.get(i));
+            }
+        }
+        for (int i = 0; i < uniqueNumbers.toArray().length; i++) {
+            int counter = 0;
+            for (int j = 0; j < words.toArray().length; j++) {
+                if(uniqueNumbers.get(i).contains(words.get(j))){
+                    counter++;
+                }
+            }
+            System.out.println(uniqueNumbers.get(i) + ": " + counter);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<String> fruits = List.of("Apple", "Orange", "Apple", "Banana", "Banana", "Orange", "Banana", "Apple", "Orange", "Orange");
+        ListsUsageExample.countOccurance(fruits, "Apple");
+
+        int[] array = {5, 4, 3, 2, 1};
+        ListsUsageExample.toList(array);
+
+        List<Integer> numbers = List.of(1, 1, 2, 3, 5, 5, 6, 3);
+        System.out.println(numbers);
+        List<Integer> changedNumbers = ListsUsageExample.findUnique(numbers);
+        System.out.println(changedNumbers);
+
+        List<String> animals = List.of("Cat", "Bird", "Bird", "Fox", "Cat", "Fox", "Fox", "Cat", "Bird", "Fox");
+        ListsUsageExample.calcOccurance(animals);
+    }
 }
